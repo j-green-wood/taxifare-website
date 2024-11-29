d2b056acc45ad959bfe89a016bb9dc9dbb3650d6
julia greenwood <j.greenwood98@outlook.com>
2024-11-29 12:04:10 -0500

    modifications

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import datetime
+import requests
+import pandas as pd
 
-st.markdown(''' # Estimate your taxi fare! 🚕💰''')
+st.markdown(''' ## Estimate your taxi fare! 🚕💰''')
 
 def get_input():
-    st.write('please enter the following information regarding your next ride:')
+    st.markdown(''' # Please enter the following information regarding your next ride:''')
     input_items = {
     'pickup time': st.date_input('Enter the pickup time: ', datetime.date(2019, 7, 6)),
     'pickup longitude': st.number_input('Enter the pickup longitude: '),
@@ -13,32 +15,14 @@ def get_input():
     'dropoff latitude': st.number_input('Enter the dropoff latitude: '),
     'passenger count': st.number_input('Enter the passenger count: ')
     }
-    st.write('Your ride details are: ', input_items.items())
+    time, pickup_longitude,pickup_latitude,dropoff_longitude, dropoff_latitude, passengers  = input_items.values()
+    st.write('Your ride details are: Time: ', time , 'Pickup Longitude: ', pickup_longitude, 'Pickup Latitude: ', pickup_latitude, 'Dropoff Longitude: ', dropoff_longitude, 'Dropoff Latitude: ', dropoff_latitude, 'Passenger Count: ', passengers)
     return input_items
-get_input()
+ride_info = get_input()
 
-
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
 
 url = 'https://taxifare.lewagon.ai/predict'
+pred = requests.get(url, ride_info).json()
+final_pred = pred['fare']
 
-if url == 'https://taxifare.lewagon.ai/predict':
-
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
-
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
+st.markdown(f'This ride will cost you: ${final_pred}')
